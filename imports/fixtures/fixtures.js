3a132b907c4734d86b69ad66122c285e5ff4554b
Trevor Pryhorocki <cuoredoro14@gmail.com>
2017-03-21 10:00:48 -0400

    Integrate selectedNoteId into list items and url

---
+++ imports/fixtures/fixtures.js
@@ -0,0 +1,15 @@
+export const notes = [
+  {
+    _id: 'id1',
+    title: 'Note 1',
+    body: '',
+    updatedAt: 1489927756466,
+    userId: 'userId1'
+  }, {
+    _id: 'id2',
+    title: '',
+    body: 'Body',
+    updatedAt: 1489927756466,
+    userId: 'userId2'
+  }
+];
--- imports/ui/NoteListItem.test.js
+++ imports/ui/NoteListItem.test.js
@@ -3,25 +3,37 @@ import {Meteor} from 'meteor/meteor';
 import expect from 'expect';
 import {mount} from 'enzyme';
 
-import NoteListItem from './NoteListItem';
+import {NoteListItem} from './NoteListItem';
+import {notes} from '../fixtures/fixtures';
 
 if(Meteor.isClient){
   describe('NoteListItem', function(){
+    let Session;
+
+    beforeEach(() => {
+      Session = {
+        set: expect.createSpy()
+      };
+    });
 
     it('should render title and timestamp', function(){
-      const title = "Note Title";
-      const updatedAt = 1489927756466;
-      const wrapper = mount(<NoteListItem note={{title, updatedAt}}/>);
+      const wrapper = mount(<NoteListItem note={notes[0]} Session={Session}/>);
 
-      expect(wrapper.find('h5').text()).toBe(title);
+      expect(wrapper.find('h5').text()).toBe(notes[0].title);
       expect(wrapper.find('p').text()).toBe('3/19/17');
     });
 
     it('should set default title if no title set', function(){
-      const updatedAt = 1489927756466;
-      const wrapper = mount(<NoteListItem note={{title: '', updatedAt}}/>);
+      const wrapper = mount(<NoteListItem note={notes[1]} Session={Session}/>);
 
       expect(wrapper.find('h5').text()).toBe('Untitled Note');
+    });
+
+    it('should call set on click', function(){
+      const wrapper = mount(<NoteListItem note={notes[0]} Session={Session}/>);
+      wrapper.find('div').simulate('click');
+
+      expect(Session.set).toHaveBeenCalledWith('selectedNoteId',notes[0]._id);
     });
   })
 }
