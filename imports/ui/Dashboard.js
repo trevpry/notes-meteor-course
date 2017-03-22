1c49de879350e1f5fc8332ee1222c722637daf03
Trevor Pryhorocki <cuoredoro14@gmail.com>
2017-03-22 11:29:56 -0400

    Setup page content layout

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 import PrivateHeader from './PrivateHeader';
 import NoteList from './NoteList';
 import Editor from './Editor'
@@ -10,9 +9,10 @@ export default Link = () => {
     <div>
       <PrivateHeader title="Dashboard"/>
       <div className="page-content">
-        Dashboard page content.
-        <NoteList/>
-        <Editor/>
+        <div className="page-content__sidebar"><NoteList/></div>
+        <div className="page-content__main"><Editor/></div>
+
+
       </div>
     </div>
   );
